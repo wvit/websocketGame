3e3abd2add31c2f9ce20b786d5e8e03cddc4278c
wvit <1999wuwei@gmail.com>
2018-09-19 21:54:38 +0800

    初识websocket

---
+++ websocket-demo/websocket-server.js
@@ -0,0 +1,11 @@
+const ws = require('nodejs-websocket');
+const server = ws.createServer((connect) => {
+  console.log('创建成功');
+  connect.on('text', str => {
+    console.log('send :' + str);
+    connect.sendText(str.toUpperCase());
+  })
+  connect.on('close', (code, reason) => {
+    console.log('已断开连接');
+  })
+}).listen(1999);
