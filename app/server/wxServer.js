5eaa96a52cfdd88d5c08b3fb9e7e6f33eb366ff0
wvit <1999wuwei@gmail.com>
2018-09-30 00:16:03 +0800

    9月30日 凌晨

--- app/server/wxServer.js
+++ app/server/wxServer.js
@@ -2,6 +2,7 @@ const app = require('http').createServer();
 const io = require('socket.io')(app);
 app.listen(1999);
 io.on('connection', socket => {
+  console.log('已连接');
   io.emit('enter', '对方加入游戏');
   socket.on('message', data => {
     console.log(data);
