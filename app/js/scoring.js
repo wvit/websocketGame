4c355175c70c2571a97f5a967d9de4490ade8a55
wvit <1999wuwei@gmail.com>
2018-10-05 14:31:16 +0800

    10月4日 下午

--- app/js/scoring.js
+++ app/js/scoring.js
@@ -1,20 +1,25 @@
 //判定是否得分
 function judgeScore() {
     for (let i = 0; i < gameDivs.length; i++) {
-        if (!judgeRow(i)) {
-            repaint(i)
+        for (let j = 0; j < gameDivs[i].length; j++) {
+            if (j === gameDivs[i].length - 1 && gameDivs[i][j].className != 'none') {
+                repaint(i)
+            }
         }
     }
 }
 
 //判断哪行已经铺满
-function judgeRow(rowIndex) {
-    for (let i = 0; i < gameDivs[rowIndex].length; i++) {
-        if (gameDivs[rowIndex][i].className === 'none') {
-            return true;
-        }
-    }
-}
+// function judgeRow(rowIndex) {
+//     for (let i = 0; i < gameDivs[rowIndex].length; i++) {
+//         if (gameDivs[rowIndex][i].className === 'none') {
+//             return false;
+//         }
+//         if (i === gameDivs[rowIndex].length - 1) {
+//             return true;
+//         }
+//     }
+// }
 
 //消除占满的那行，并且上面的方块下落
 function repaint(rowIndex) {
