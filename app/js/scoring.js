519ed38d343c804e24f109460b13135b3e655b76
wvit <1999wuwei@gmail.com>
2018-10-06 14:10:26 +0800

    10月6号 中午

--- app/js/scoring.js
+++ app/js/scoring.js
@@ -1,32 +1,42 @@
 //判定是否得分
+
 function judgeScore() {
+    let removeRow = []
     for (let i = 0; i < gameDivs.length; i++) {
         for (let j = 0; j < gameDivs[i].length; j++) {
             if (j === gameDivs[i].length - 1 && gameDivs[i][j].className != 'none') {
-                repaint(i)
+                removeRow.push(i);
             }
         }
     }
+    repaint(removeRow)
 }
 
-//判断哪行已经铺满
-// function judgeRow(rowIndex) {
-//     for (let i = 0; i < gameDivs[rowIndex].length; i++) {
-//         if (gameDivs[rowIndex][i].className === 'none') {
-//             return false;
-//         }
-//         if (i === gameDivs[rowIndex].length - 1) {
-//             return true;
-//         }
-//     }
-// }
-
 //消除占满的那行，并且上面的方块下落
-function repaint(rowIndex) {
-    for (let i = 0; i < gameDivs[rowIndex].length; i++) {
-        gameDivs[rowIndex][i].className = 'none'
-    }
-    gameData.splice(rowIndex, 1);
-    gameData.unshift(rowNull);
-    drawing(gameData, gameDivs);
+function repaint(row) {
+    let a = 0;
+    let b = 0;
+    row.forEach(value => {
+        for (let i = 0; i < gameDivs[value].length; i++) {
+            gameDivs[value][i].className = 'none';
+        }
+        // gameData.splice(value, 1);
+        // gameData.unshift(rowNull);
+        // drawing(gameData, gameDivs);
+    });
+
+
+
+    // for (let i = 0; i < row.length; i++) {
+    //     for (let j = 0; j < gameDivs[row[i]].length; j++) {
+    //         gameDivs[row[i]][j].className = 'none';
+    //     }
+    // }
+
+    // for (let i = 0; i < row.length; i++) {
+    //     gameData.splice(row[i], 1);
+    //     gameData.unshift(rowNull);
+    //     drawing(gameData, gameDivs);
+    // }
+
 }
